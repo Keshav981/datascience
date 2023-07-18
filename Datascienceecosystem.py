73ac66beb21563c6f4f5e70497e88d37f33b3bb5
Keshav981 <87293074+Keshav981@users.noreply.github.com>
2023-07-18 18:59:51 +0530

    Add files via upload

---
+++ Datascienceecosystem.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print("DataScienceEcosystem.ipynb")
+
+
+# In[6]:
+
+
+print("Datasceince tool and ecosystem")
+print("1.IDE jypyter notebbok,jupyter lab,R studio,Vs code,")
+print("programming languages a)R ,python,sql,julia,scal")
+print("Libraries and Packages Pandas,NumPyMatplotlibSeabornggplot2Scikit-learnTensorFlowPyTorch")
+
+
+# In[7]:
+
+
+print("data science languages are : python,R ,sql julia,scal")
+
+
+# In[9]:
+
+
+import pandas as pd
+
+# Data Science tools data
+data = {
+    'Data Science Tools': [
+        'Jupyter Notebook',
+        'RStudio',
+        'Spyder',
+    ]
+}
+
+
+df = pd.DataFrame(data)
+
+# Display the DataFrame as a table
+print(df)
+
+
+# In[10]:
+
+
+3+3
+3*3
+10%2
+10-2
+
+
+
+# In[11]:
+
+
+min=200
+hour=min/60
+print(hour)
+
+
+# In[15]:
+
+
+print("popular language for data science")
+print("python"
+      "java"
+     )
+
+
+# In[16]:
+
+
+print("author name =keshav")
+
+
+# In[ ]:
+
+
+
+
